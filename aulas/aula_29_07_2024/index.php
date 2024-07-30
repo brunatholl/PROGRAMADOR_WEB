f8c8a787612e056ba3bb1fc3089d24cf59dc26ab
Bruna Tholl <bruna.tholl.ti@gmail.com>
2024-07-30 19:00:17 -0300

    ok

---
+++ aulas/aula_29_07_2024/index.php
@@ -0,0 +1,20 @@
+
+<html>
+	<head>
+		<title>Script de exemplo</title>
+		<meta charset="UTF-8"></meta>
+	</head>
+	<body>		
+		<?php
+		    echo "Hello PHP!";
+		?>
+	</body>
+</html>
+
+<?php
+
+echo 'Hello World';
+
+
+
+// phpinfo();
