406f3131b309d1d99641b5a03be9f059381e2d97
brunatholl <bruna.tholl.ti@gmail.com>
2024-08-06 21:38:10 -0300

    ok

--- aulas/aula_05_08_2024/Exercicio05.php
+++ aulas/aula_05_08_2024/Exercicio05.php
@@ -1,9 +1,23 @@
 <?php
 
-$frase = 'Verificar se o número é par ou ímpar: ';
-echo $frase;
-$numero = 2;
+// Regra para numero ser par:
+// Resto da divisão por 2 deve ser zero
 
-function VerificaParOuImpar ($numero){
+function isImpar($numero){
+    return $numero % 2 != 0;
+}
 
-};
+$numero = 9;
+if(isImpar($numero)){
+    echo " O número " . $numero ." é impar!";
+} else {
+    echo " O número " . $numero ." é par!";
+}
+
+echo '<hr>';
+$numero = 4;
+if(isImpar($numero)){
+    echo " O número " . $numero ." é impar!";
+} else {
+    echo " O número " . $numero ." par!";
+}
